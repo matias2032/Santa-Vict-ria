ed32673d8f78b74dd76022971a09c9b79145c438
Matias Matavel <matiasmatavel@gmail.com>
2026-09-04 09:36:59 +0200

    ultimate carx2

--- includes/header.php
+++ includes/header.php
@@ -51,7 +51,7 @@ function urlComIdioma(string $idiomaAlvo, string $paginaAtual): string {
 <link rel="icon" href="assets/images/logo.png" type="image/png">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400&family=Nunito+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700;800&family=Figtree:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
